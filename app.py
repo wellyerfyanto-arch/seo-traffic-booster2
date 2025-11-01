80dfdb2a7db8e191b4d99e4a00e7243c375b1a6d
wellyerfyanto-arch <wellyerfyanto@gmail.com>
2025-11-01 15:37:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -474,7 +474,7 @@ def main():
     # Warning box
     st.markdown("""
     <div class="warning-box">
-        <strong>⚠️ Disclaimer:</strong> Aplikasi ini adalah simulator untuk tujuan edukasi. 
+        <strong> Disclaimer:</strong> Aplikasi ini adalah simulator untuk tujuan edukasi. 
         Beberapa fitur seperti automation browser tidak tersedia di Streamlit Share karena batasan keamanan.
     </div>
     """, unsafe_allow_html=True)
